c9e3cab8a7ed3ab00a5f78d8132bc1b97b1c48d2
alearr1ola <20253ds138@utez.edu.mx>
2026-03-31 19:56:29 -0600

    Agregamos las funcionalidades de agregar y eliminar libros

--- IntegradoraEquipo9/src/main/java/utez/edu/mx/integradoraequipo9/controller/LibroController.java
+++ IntegradoraEquipo9/src/main/java/utez/edu/mx/integradoraequipo9/controller/LibroController.java
@@ -6,6 +6,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
 import utez.edu.mx.integradoraequipo9.model.Libro;
 
 public class LibroController {
@@ -13,9 +17,12 @@ public class LibroController {
     @FXML
     private TableView<Libro> tableLibros;
 
+    private ObservableList<Libro> listaLibros = FXCollections.observableArrayList();
+
     @FXML
     public void agregarLibro() {
-        System.out.println("Nuevo");
+        Libro nuevo = new Libro("4", "Nuevo Libro", "Autor X", 2024, "Género", true);
+        listaLibros.add(nuevo);
     }
 
     @FXML
@@ -23,9 +30,18 @@ public class LibroController {
         System.out.println("Editar");
     }
 
+    /**
+     * Eliminamos el libro seleccionado de la tabla.
+     */
     @FXML
     public void eliminarLibro() {
-        System.out.println("Eliminar");
+        Libro libro = tableLibros.getSelectionModel().getSelectedItem();
+
+        if (libro != null) {
+            listaLibros.remove(libro);
+        } else {
+            System.out.println("Selecciona un libro");
+        }
     }
 
     @FXML
@@ -51,14 +67,12 @@ public class LibroController {
         colGenero.setCellValueFactory(new PropertyValueFactory<>("genero"));
         colDisponible.setCellValueFactory(new PropertyValueFactory<>("disponible"));
 
-        ObservableList<Libro> lista = FXCollections.observableArrayList(
+        listaLibros.addAll(
                 new Libro("1", "Harry Potter", "Rowling", 2001, "Fantasía", true),
                 new Libro("2", "1984", "Orwell", 1949, "Distopía", true),
                 new Libro("3", "It", "Stephen King", 1986, "Terror", false)
         );
 
-        tableLibros.setItems(lista);
+        tableLibros.setItems(listaLibros);
     }
-
 }
-
